05fc0e71dfb4dc29ba29c4481e59c1b0cd7aef9c
Niranjan <niranjan17091998@gmail.com>
2021-10-17 17:17:02 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+function connectDB(){
+
+    //Database connection 
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useCreateIndex:true,
+        useFindAndModify:true
+    });
+
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected..')
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+module.exports = connectDB
